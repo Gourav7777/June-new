312fcd3140a3b818e6af77af4629b27397253d0b
Gourav7777 <360inside360@gmail.com>
2025-07-16 17:35:27 +0530

    d

---
+++ Javascript/Execution-Context/p.js
@@ -0,0 +1,33 @@
+var a = 10
+let b = 20
+const c = 30
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+if(true){
+
+    var d = 20
+    let b = 10
+    const c = 10
+
+console.log(d);
+console.log(b);
+console.log(c);
+      
+}
+
+
+function add(){
+       var a = 20
+    let b = 10
+    const c = 10
+
+console.log(a);
+console.log(b);
+console.log(c);
+}
+
+add()
